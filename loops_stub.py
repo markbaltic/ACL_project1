e92e172fbfcfff31a10e6555d03bb89144f60179
markbaltic <mark.baltic@gmail.com>
2019-10-28 10:34:12 +0000

    From lab 23 oct

--- loops_stub.py
+++ loops_stub.py
@@ -21,10 +21,10 @@ class Enc:
 
     def x(self,i): return 'x_{}'.format(i)
     def y(self,i): return 'y_{}'.format(i)
-    def v(self,i): return 'v_{}'.format(i)
-    def l(self,i,j): return 'l_{}_{}'.format(i,j)
-    def r(self,i,j): return 'r_{}_{}'.format(i,j)
-    def p(self,j,i): return 'p_{}_{}'.format(i,j)
+    def v(self,i): return 'v_{}'.format(i) # 1 iff node i is a leaf node, i = 1, . . . , N
+    def l(self,i,j): return 'l_{}_{}'.format(i,j) # 1 iff node i has node j as the left child, with j ∈ LR (i)
+    def r(self,i,j): return 'r_{}_{}'.format(i,j) # 1 iff node i has node j as the right child, with j ∈ RR (i)
+    def p(self,j,i): return 'p_{}_{}'.format(i,j) # 1 iff the parent of node j is node i
     def a(self,r,j): return 'a_{}_{}'.format(r,j)
     def u(self,r,j): return 'u_{}_{}'.format(r,j)
     def c(self,j): return 'c_{}'.format(j)
@@ -38,12 +38,13 @@ class Enc:
         else:
             return ("Wrong feature value!!!!!")
   
-	
+
     
 
     def add_constraint(self, constraint):
         '''add constraints, which is a list of literals'''
         self.constraints.append(constraint)
+
 
     def mk_fresh(self, nm):
         '''make fresh variable'''
@@ -67,8 +68,16 @@ class Enc:
         '''add constrains to satisfy at most one for list l'''
         for x,y in combinations(l,2):
             self.add_constraint([neg(x),neg(y)])
-        
 
+    def mk_OR(self, l): # takes a list of clauses
+        '''encode and between l1 and l2 by introducing a fresh variable'''
+        r = self.mk_fresh('_OR_'.join(l))
+        for clause in l:
+            self.add_constraint([neg(clause), r])
+
+        self.add_constraint([neg(r)]+ l)
+        return r
+        
 
     def print_model(self,model):
         '''prints SAT model, eventually should print the decision tree'''
@@ -80,6 +89,19 @@ class Enc:
             if v in model and not model[v]: val='F'
             print('# {}={} ({})'.format(str_var,val,v))
         print('# === end of model')
+        print("START TREE")
+        for str_var in sorted(self.var_map.keys()):
+            v = self.var_map[str_var]
+            if str_var[0] in 'lra' and model[v]:
+                name = str_var.split("_")
+                print(name[0], name[1], name[2])
+            elif str_var[0]=='c':
+                name = str_var.split("_")
+                node = name[1]
+                leaf = self.var_map["v_" + node]
+                if leaf in model and model[leaf]:
+                    result = 1 if model[v] else 0
+                    print(name[0], name[1], result)
         print('# === tree (TODO)')
         print('# === end of tree')
 
@@ -118,73 +140,100 @@ class Enc:
             return [j for j in range(i+2,min(2*i+1,self.node_count)+1) if j%2 == 1]
 
         #Constrains for binary tree
-        #(1)
+        #(1) the root node is not a leaf
         self.add_constraint([neg(self.v(1))])
         
-        #(2)
+        #(2) If a node is a leaf node, then it has no children:
         for i in range(1,self.node_count+1):
             for j in LR(i):
                 self.add_constraint([neg(self.v(i)),neg(self.l(i,j))])
-                
-		#(3)
+
+        #(3) The left child and the right child of the ith node are numbered consecutively
         for i in range(1,self.node_count+1):
-            for j in LR(i):  
+            for j in LR(i):
                 self.add_iff(self.l(i,j),self.r(i,j+1))
                     
-        #(4)  
-        for i in range(1,self.node_count+1):  
+        #(4) An internal node must have a child.
+        for i in range(1,self.node_count+1):
             self.add_atmost_one([self.l(i,j) for j in LR(i)])
-            self.add_constraint([self.l(i,j) for j in LR(i)]+[self.v(i)])
-        
-        #(5)
+            self.add_constraint([self.l(i,j) for j in LR(i)]+[self.v(i)]) # at least one constraint
+
+
+        #(5) If the i-th node is a parent then it must have a child
         for i in range(1,self.node_count+1):
             for j in LR(i):    
                 self.add_iff(self.p(i,j),self.l(i,j))
             for j in RR(i):
                 self.add_iff(self.p(i,j),self.r(i,j))
                 
-        #(6)
+        #(6) The binary tree must be a tree. Hence, all nodes but the first must have a parent:
         for j in range(2,self.node_count+1):
             P = [self.p(i,j) for i in range(int(j/2),min(j-1,self.node_count)+1)]
             self.add_atmost_one(P)
             self.add_constraint(P)
-		
+
 
                 
         #Constrains for features
-		#index of feature is denoted with "k"
-
-		#(7)
+        #index of feature is denoted with "k"
+        """
         for k in range(1,self.input_count+1):
             self.add_constraint([neg(self.d0(k,1))])
             for j in range(2,self.node_count+1):
-                if j%2 == 1:
-                    list_j = []
-                    for i in range(int(j/2),j): #right implicaton
-                        list_j.append(self.mk_and(self.p(i,j),self.d0(k,i))) 
+                list_j = []
+                for i in range(int(j/2), j):
+                    list_j.append(self.mk_and(self.p(i,j),self.d0(k,i)))
+                    if j in RR(i):
                         list_j.append(self.mk_and(self.a(k,i),self.r(i,j)))
-                    self.add_constraint([neg(self.d0(k,i))]+list_j)
-                    
-                    for i in range(int(j/2),j): #left implication 
-                        self.add_constraint([neg(self.p(i,j)),neg(self.d0(k,i)),self.d0(k,j)])
+
+                self.add_constraint(self.add_iff(self.d0(k,j), list_j))
+
+        """
+
+
+        #(7)
+        for k in range(1,self.input_count+1):
+            self.add_constraint([neg(self.d0(k,1))])
+            for j in range(2,self.node_count+1):
+                list_j = []
+                for i in range(int(j/2),j): # create list for making the big OR over
+                    list_j.append(self.mk_and(self.p(i,j),self.d0(k,i)))
+                    if j in RR(i):
+                        list_j.append(self.mk_and(self.a(k, i), self.r(i, j)))
+
+                self.add_iff(self.d0(k,j), self.mk_OR(list_j))
+
+                """
+                self.add_constraint([neg(self.d0(k,i))]+list_j)
+                
+                for i in range(int(j/2),j): #left implication
+                    self.add_constraint([neg(self.p(i,j)),neg(self.d0(k,i)),self.d0(k,j)])
+                    if j in RR(i):
                         self.add_constraint([neg(self.a(k,i)),neg(self.r(i,j)),self.d0(k,j)])
-        
+                """
+
         #(8)
         for k in range(1,self.input_count+1):
             self.add_constraint([neg(self.d1(k,1))])
             for j in range(2,self.node_count+1):
-                if j%2 == 0:
-                    list_j = []
-                    for i in range(int(j/2),j): #right implicaton
-                        list_j.append(self.mk_and(self.p(i,j),self.d1(k,i))) 
+                list_j = []
+                for i in range(int(j/2),j): #right implicaton
+                    list_j.append(self.mk_and(self.p(i,j),self.d1(k,i)))
+                    if j in LR(i):
                         list_j.append(self.mk_and(self.a(k,i),self.l(i,j)))
-                    self.add_constraint([neg(self.d1(k,i))]+list_j)
-                    
-                    for i in range(int(j/2),j): #left implication 
-                        self.add_constraint([neg(self.p(i,j)),neg(self.d1(k,i)),self.d1(k,j)])
-                        self.add_constraint([neg(self.a(k,i)),neg(self.l(i,j)),self.d1(k,j)])
 
-        #(9.1)        
+                self.add_iff(self.d1(k, j), self.mk_OR(list_j))
+
+                """
+                self.add_constraint([neg(self.d1(k,i))]+list_j)
+
+                for i in range(int(j/2),j): #left implication
+                    self.add_constraint([neg(self.p(i,j)),neg(self.d1(k,i)),self.d1(k,j)])
+                    if j in LR(i):
+                        self.add_constraint([neg(self.a(k,i)),neg(self.l(i,j)),self.d1(k,j)])
+                """
+
+        #(9.1)
         for k in range(1,self.input_count+1):
             for j in range(1, self.node_count+1):
                 for i in range(max(1,int(j/2)),j): ####is the begining of range ok???
@@ -198,12 +247,12 @@ class Enc:
                 self.add_constraint([self.u(k,j),neg(self.a(k,j))]) #left implication
                 for i in range(max(1,int(j/2)),j): #left implication
                     self.add_constraint([self.u(k,j),neg(self.u(k,i)),neg(self.p(i,j))])
-        
+
+
         #(10)
-        for k in range(1,self.input_count+1):
-            for j in range(1, self.node_count+1):
-                self.add_atmost_one([self.a(k,j) for k in range(1,self.input_count+1)])
-                self.add_constraint([self.a(k,j) for k in range(1,self.input_count+1)]+[self.v(j)])
+        for j in range(1, self.node_count+1):
+            self.add_atmost_one([self.a(k,j) for k in range(1,self.input_count+1)])
+            self.add_constraint([self.a(k,j) for k in range(1,self.input_count+1)]+[self.v(j)])
 
         #(11)
         for k in range(1,self.input_count+1):
@@ -229,8 +278,8 @@ class Enc:
                 self.add_constraint([neg(self.l(i,j))])
             for j in LR(i):
                 self.add_constraint([neg(self.r(i,j))])
-            
-        
+
+     
 #        # -x1 | -x2
 #        self.add_constraint([neg(self.x(1)), neg(self.x(2))])
 #        # x1 | x2
@@ -241,7 +290,7 @@ class Enc:
 #        self.add_constraint([self.y(1), self.mk_and(self.y(2),self.y(3))])
 #        # -y1
 #        self.add_constraint([neg(self.y(1))])
-#        
+#        """
         
 def get_model(lns):
     vals=dict()
